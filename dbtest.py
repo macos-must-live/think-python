ceb257a28ce41b767a9313318333c99b113962b4
ruzzet_laptop <dr.ruzzet@yandex.ru>
2024-06-26 20:55:25 -0400

    tests

--- dbtest.py
+++ dbtest.py
@@ -7,6 +7,9 @@ import math
 
 db = shelve.open("test.db", "c")
 
+print(hash(u"D:/entertainment/music/Aria\Легенды русского рока\Игра с огнем.mp3"))
+print(u"D:/entertainment/music/Aria\Легенды русского рока\Игра с огнем.mp3".encode("utf-16le"))
+
 try:
     # pickle.dumps/pickle.loads
     print(db["test"])
@@ -14,7 +17,10 @@ try:
     print(db["first"])
     print(db["f"].get(1, "[]"))
     print(db["f"])
+    
     print(db.get("first", "no data"))
+    print(db.get(u"D:/entertainment/music/Aria\Легенды русского рока\Игра с огнем.mp3"))
+    # print(db.get((u"D:/entertainment/music/Aria\Легенды русского рока\Игра с огнем.mp3".encode("utf-16le"))))
 except Exception as ex:
     print(f"error: {ex=}")
 
@@ -27,5 +33,7 @@ db["test"] = "new testdb"
 db["test1"] = [1,2,3]
 db["first"] = (1,2,3)
 db["f"] = {1:"2", 2:"3"}
+db[u"D:/entertainment/music/Aria\Легенды русского рока\Игра с огнем.mp3"] = "test"
+# db[u"D:/entertainment/music/Aria\Легенды русского рока\Игра с огнем.mp3".encode("utf-16le")] = "test1"
 # finally: 
 db.close()
--- md5checksum.py
+++ md5checksum.py
@@ -13,28 +13,45 @@ def walk_files(script, dir, *rest):
     verbose = "--verbose" in rest
     rehash = "--rehash" in rest
 
+
+    if "--check" in sys.argv: 
+        print(f"total files count: {str(len(shelve_db['files'].keys()))}")
+        print(f"total checksums count: {str(len(shelve_db['checksums'].keys()))}")
+        print(shelve_db["files"].keys())
+        print(shelve_db["files"].get(u"D:/entertainment/music/Aria\Легенды русского рока\Игра с огнем.mp3"))
+        print(u"D:/entertainment/music/Aria\Легенды русского рока\Игра с огнем.mp3".replace("\\", "\\\\"))
+        print("bye!")
+        return 
+
+
     print(f"processing {dir}", " with rehash" if rehash else "")
     for root, dirname, files in os.walk(dir,True):
         for f in files:
             file = os.path.join(root,f)
+            # encoded_file = file.encode("utf-16le")
+            encoded_file = file
+
             if verbose: print(f"{file}", end=" ")
             lastmtime = os.path.getmtime(file)
-            savedmtime,checksum = shelve_db["files"].get(file, (0, ''))
+            savedmtime,checksum = shelve_db["files"].get(encoded_file, (0, ''))
 
             # print(f"saved {(savedmtime, checksum)} vs real ({lastmtime})")
 
             if savedmtime < lastmtime or rehash:
+                print(f"calculating checksum {savedmtime} {lastmtime} {savedmtime < lastmtime} {rehash} {file}")
                 checksum = md5(file, "--debug" in rest)
+                
                 if verbose: print(f" -> {checksum}")
                 if checksum in shelve_db["checksums"]:
-                    shelve_db["checksums"][checksum].append(file)
+                    shelve_db["checksums"][checksum].append(encoded_file)
                 else:
-                    shelve_db["checksums"][checksum] = [file]
+                    shelve_db["checksums"][checksum] = [encoded_file]
             else:
                 if verbose: print(f"-> picked saved checksum {(savedmtime,checksum)}")
 
-            if len(checksum)>0 and file not in shelve_db["files"]: 
-                shelve_db["files"][file] = (lastmtime, checksum)
+            if len(checksum)>0 and encoded_file not in shelve_db["files"]: 
+                shelve_db["files"][encoded_file] = (lastmtime, checksum)
+                print(f"saving last modif time of {file} to cache")
 
             shelve_db.sync()
 
@@ -66,9 +83,11 @@ def md5(file, debug=True):
 
 if __name__ == "__main__":
     # opendb()
+    print(f"starting from {sys.path}")
     shelve_db = shelve.open("checksums.db", "c", writeback=True)
     shelve_db["files"] = shelve_db.get("files", dict())
     shelve_db["checksums"] = shelve_db.get("checksums", dict())
+        
     # shelve_db["files"][3] = "test"
     # print(shelve_db["files"] )
     try:
